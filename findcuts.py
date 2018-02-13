5acec0b1d4e1a66a992ae457294eb3fbaeba52f0
James Preiss <japreiss@usc.edu>
2018-02-12 20:58:26 -0800

    use terms consistently, file saving, json

--- findcuts.py
+++ findcuts.py
@@ -7,6 +7,7 @@ import os
 import sys
 import time
 
+from collections import namedtuple
 import cv2
 import itertools
 import json
@@ -20,10 +21,15 @@ import pygame
 #
 # terminology used:
 # -----------------
-# cut  - a single moment in time when the shot changes
-# clip - (begin, end) time boundaries of a shot
+# video - a full multi-shot video, to be split
+# cut   - a single moment in time when the shot changes
+# clip  - (begin, end) time boundaries of a shot
 #
 
+Clip = namedtuple('Clip', 'begin end')
+
+# input: array of arrays, each same length, any type
+# output: a table with justified columns, as array of strings
 def tabular(rows):
 	if len(rows) == 0:
 		return
@@ -38,21 +44,23 @@ def tabular(rows):
 		cells = (s.ljust(cmax) for s, cmax in zip(row, colmax))
 		yield " ".join(cells)
 
-def print_info(clip):
+# pretty-print some basic facts about the video.
+def print_info(video):
 	rowstrs = list(tabular([
-		["name:", clip.filename],
-		["size:", clip.size],
-		["duration:", str(clip.duration) + " secs"],
-		["fps:", clip.fps],
+		["name:", video.filename],
+		["size:", video.size],
+		["duration:", str(video.duration) + " secs"],
+		["fps:", video.fps],
 	]))
 	underscore_len = max(len(row) for row in rowstrs)
 	print("")
-	print("clip info:")
+	print("video info:")
 	print("-" * underscore_len)
 	for s in rowstrs:
 		print(s)
 	print("")
 
+# random sample of N (size, size) tiles in the middle of the image
 def sample_tiles(img, size, N):
 	h, w, _ = img.shape
 	h_range, w_range = ((int(0.25 * x), int(0.75 * x)) for x in (h, w))
@@ -62,14 +70,16 @@ def sample_tiles(img, size, N):
 		return x, y, img[y:(y+size),x:(x+size),:]
 	return [make_tile() for _ in range(N)]
 
-def find_likely_cuts(clip, n_top=None, tile_size=8, n_tiles=20, radius=15):
-	dt = 1.0 / clip.fps
+# TODO this can be done better. use an optical flow
+# or multi-view geometric method to recognize camera movements.
+def find_likely_cuts(video, n_top=None, tile_size=8, n_tiles=20, radius=15):
+	dt = 1.0 / video.fps
 	match_sums = []
 	if n_top is None:
 		# most FPV video does not contain fast cuts.
 		# might need to increase n_top for other kinds of video.
-		n_top = int(clip.duration)
-	frames = clip.iter_frames(progress_bar=True)
+		n_top = int(video.duration)
+	frames = video.iter_frames(progress_bar=True)
 	prev_frame = None
 	for i, frame in enumerate(frames):
 		# TODO cleanup
@@ -95,15 +105,15 @@ def find_likely_cuts(clip, n_top=None, tile_size=8, n_tiles=20, radius=15):
 # FPV videos do not have extremely rapid shots,
 # so if we detect two "cuts" nearby, it's more likely
 # that we're in an extreme roll
-def remove_nearby(times):
-	times = np.array(times)
-	for t in times:
-		delta = times - t
+def remove_nearby(cuts):
+	cuts = np.array(cuts)
+	for t in cuts:
+		delta = cuts - t
 		close = np.logical_and(np.abs(delta) < 1.0, delta != 0)
 		if close.any():
-			others = np.delete(times, np.where(close))
+			others = np.delete(cuts, np.where(close))
 			return remove_nearby(others)
-	return list(times)
+	return list(cuts)
 
 decide_frame = None
 
@@ -112,6 +122,8 @@ def pg_events_forever():
 		for ev in pygame.event.get():
 			yield ev
 
+# display message asking user to approve or reject cut via keyboard input.
+# input: resolution of video. output: 'y' - yes, 'n' - no, 'r' - replay
 def get_user_decision(size):
 	global decide_frame
 	if not decide_frame:
@@ -130,21 +142,21 @@ def get_user_decision(size):
 		if ev.type == pygame.KEYDOWN and ev.key in key2char:
 			return key2char[ev.key]
 
-
-def show_padded_clips(clip, times, n_repeat=1, pad_sec=1.0):
-	dt = 1.0 / clip.fps
-	black = mpe.ColorClip(clip.size, (0, 0, 0))
-	accept = [None for _ in times]
+# 
+def show_padded_cuts(video, cuts, n_repeat=1, pad_sec=1.0):
+	dt = 1.0 / video.fps
+	black = mpe.ColorClip(video.size, (0, 0, 0))
+	accept = [None for _ in cuts]
 	i = 0
-	while i < len(times):
-		t = times[i]
+	while i < len(cuts):
+		t = cuts[i]
 		t0, t1 = t - pad_sec, t + pad_sec
-		c = clip.subclip(t0, t1)
+		c = video.subclip(t0, t1)
 
 		for _ in range(n_repeat):
-			c.preview(fps=clip.fps)
+			c.preview(fps=video.fps)
 
-		u = get_user_decision(clip.size)
+		u = get_user_decision(video.size)
 		if u == 'y':
 			print("accepting cut")
 			accept[i] = True
@@ -163,75 +175,115 @@ def show_padded_clips(clip, times, n_repeat=1, pad_sec=1.0):
 			assert False
 	return accept
 
-# generates objects suitable for JSON serialization
-def cuts2clips(begin, end, cut_times, accept):
+# input: clip begin&end, list of proposed cuts, user's accept/reject decisions
+# yields: NamedTuples of (begin, end) defining clips
+def cuts2clips(begin, end, cuts, accept):
 	clips = []
 	tprev = begin
-	for t, acc in zip(cut_times, accept):
+	for t, acc in zip(cuts, accept):
 		if acc:
-			yield { "begin" : tprev, "end" : t }
+			yield Clip(tprev, t)
 			tprev = t
-	yield { "begin" : tprev, "end" : end }
+	yield Clip(tprev, end)
 
-def save_results(filename, clip, cut_times, accept):
-	begin = 0 # TODO get from clip
-	end = clip.duration
-	cuts = list(cuts2clips(begin, end, cut_times, accept))
-	obj = {
-		"filename" : clip.filename,
-		"cuts" : cuts,
-	}
-	with open(filename, 'w') as f:
-		json.dump(obj, f)
-
-def approve_clips(clip, cuts):
-	black = mpe.ColorClip(clip.size, (0, 0, 0))
-	accept = [True for _ in cuts]
-	for i, c in enumerate(cuts):
-		t0, t1 = c["begin"], c["end"]
-		subclip = clip.subclip(t0, t1)
+def approve_clips(video, clips):
+	black = mpe.ColorClip(video.size, (0, 0, 0))
+	accept = [True for _ in clips]
+	for i, c in enumerate(clips):
+		clip = video.subclip(*c)
 		reject_keys = [pygame.K_DELETE, pygame.K_BACKSPACE]
 		def event_handler(ev):
 			if ev.type == pygame.KEYDOWN and ev.key in reject_keys:
-				print("rejecting clip")
+				print("rejecting video")
 				accept[i] = False
-		subclip.preview(fps=clip.fps, event_handler=event_handler)
+		clip.preview(fps=video.fps, event_handler=event_handler)
 		black.show()
 		time.sleep(1)
 	return accept
 
+def save_clips(video, clips):
+	name = video.filename
+	dirpath = "./{}_clips".format(name)
+	if not os.path.exists(dirpath):
+		os.mkdir(dirpath)
+	for c in clips:
+		path = "{}/{}{:.4f}_{:.4f}.mp4".format(dirpath, name, *c)
+		video.subclip(*c).write_videofile(path)
+
+#
+# JSON "schema": {
+#   filename: <str>
+#   proposed_cuts: [floats]
+#   accept: [bools] or null
+#   clips: [[t0, t1], ...] or null
+#
+def save_results(video, cuts, accept=None, clips=None):
+	obj = {
+		"filename" : video.filename,
+		"proposed_cuts" : cuts,
+		"accept" : accept,
+		"clips" : clips,
+	}
+	with open(video.filename + '.json', 'w') as f:
+		json.dump(obj, f)
+
+def load_or_construct_results(video_filename):
+	path = video_filename + ".json"
+	if os.path.exists(path):
+		with open(path) as f:
+			return json.load(f), path
+	results = {
+		"filename" : video_filename,
+		"proposed_cuts" : None,
+		"accept" : None,
+		"clips" : None,
+	}
+	return results, path
+
+def user_cancel(msg):
+	cmd = raw_input(msg + "\nenter to continue, q to exit.\n")
+	return cmd in ['q', 'Q']
+
 def main():
 	argparser = argparse.ArgumentParser()
-	argparser.add_argument('clip')
+	argparser.add_argument('video')
 	args = argparser.parse_args()
 
 	# TODO convert 60fps videos to 30fps?
-	clip = mpe.VideoFileClip(args.clip, 
-		target_resolution=(320, None),
-		audio=False)
-	print_info(clip)
+	video = mpe.VideoFileClip(args.video, 
+		target_resolution=(320, None), audio=False)
+	video_lowres = mpe.VideoFileClip(
+		args.video, target_resolution=(64, None), audio=False)
+	print_info(video)
 
-	if not os.path.exists('./cut_hyps'):
-		clip_lowres = mpe.VideoFileClip(
-			args.clip, target_resolution=(64, None), audio=False)
-		times = find_likely_cuts(clip_lowres)
-		times = remove_nearby(times)
-		pickle.dump(times, open('./cut_hyps', 'wb'))
-	else:
-		times = pickle.load(open('./cut_hyps', 'rb'))
+	results, path = load_or_construct_results(args.video)
+	if not results["proposed_cuts"]:
+		if user_cancel("search for proposed cuts?"):
+			return
+		cuts = find_likely_cuts(video_lowres)
+		cuts = sorted(remove_nearby(cuts))
+		results["proposed_cuts"] = cuts
+		with open(path, 'w') as f:
+			json.dump(results, f)
+	if not results["accept"]:
+		if user_cancel("review proposed cuts?"):
+			return
+		cuts = results["proposed_cuts"]
+		accept = show_padded_cuts(video, cuts)
+		clips = list(cuts2clips(0, video.duration, cuts, accept))
+		results["accept"] = accept
+		results["clips"] = clips
+		with open(path, 'w') as f:
+			json.dump(results, f)
 
-	times = sorted(times)
+	clips = results["clips"]
+	if user_cancel("review clips?"):
+		return
+	#keep = approve_clips(video, clips)
 
-	if False:
-		accept = show_padded_clips(clip, times)
-		cuts = cuts2clips(0, clip.duration, times, accept)
-		save_results('test.json', clip, times, accept)
-	else:
-		j = json.load(open('test.json'))
-		cuts = j["cuts"]
-
-	approve_clips(clip, cuts)
-
+	if user_cancel("save clips?"):
+		return
+	save_clips(video_lowres, clips)
 
 if __name__ == '__main__':
 	main()
